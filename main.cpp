24db41b538059a71d7b5277fea791fc5da1087db
olligobber <sirolligobbervii@yahoo.com.au>
2015-03-19 22:20:11 +1100

    Attempted animation

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 
-void display(void)
-{
+static int fade = 0;
+
+void display(void) {
     glClear (GL_COLOR_BUFFER_BIT);
-    glColor3f (1.0, 0.5, 0.5);
+    glColor3f (0.5 + fade/2000.0, 0.5 + fade/2000.0, 1.0);
     glBegin(GL_POLYGON);
         glVertex3f (-0.5, -0.5, 0.0);
         glVertex3f (-0.5, 0.5, 0.0);
         glVertex3f (0.5, 0.5, 0.0);
         glVertex3f (0.5, -0.5, 0.0);
     glEnd();
-    glFlush ();
+    glutSwapBuffers();
+}
+
+void idle(void) {
+    fade++;
+    if (fade > 1000) fade = 0;
+    glutPostRedisplay();
 }
 
 int main (int argc, char** argv) {
@@ -25,6 +32,7 @@ int main (int argc, char** argv) {
     glLoadIdentity();
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
     glutDisplayFunc(display);
+    glutIdleFunc(idle);
     glutMainLoop();
     return 0;
 }
